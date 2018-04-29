7403f103b6a967f95652cf4d0e45a21eca782c39
kr2842 <kr2842@gmail.com>
2018-04-29 20:45:07 +0900

    tested the algorithm

--- check_performance.py
+++ check_performance.py
@@ -72,17 +72,73 @@ def get_score(origin, reduced):
     return ret / lcmc_max_val_rank_range
 
 
-LOWER_DIMENSION = 3
+def compare_reuction_results(origin, reduced_data1, reduced_data2):
+    score1 = get_approximate_score(array, reduced_data1)
+    score2 = get_approximate_score(array, reduced_data2)
+
+    if score1 > score2:
+        return 1
+
+    return 2
+
+
+# a simple algorithm that just selects first n dimensions
+def simple_dimension_reduction_algorithm(input_array, num_components):
+
+    return input_array[:, :min(input_array.shape[1], num_components)]
+
 
 if __name__ == "__main__":
     df = pd.read_csv('creditcard.csv', sep=',', header=0)
     df.fillna(0, inplace=True)
     array = df.as_matrix()
 
-    pca = PCA(n_components=min(LOWER_DIMENSION, array.shape[1]), copy=False)
-    pca.fit(array)
-    pca_result = pca.transform(array)
-    print(pca_result.shape)
+    for dim in range(1, 10):
+        pca = PCA(n_components=min(dim, array.shape[1]), copy=False)
+        pca.fit(array)
+        pca_result = pca.transform(array)
+        simple_result = simple_dimension_reduction_algorithm(array, dim)
 
-    # print(get_score(array[:25, :], pca_result[:25, :]))
-    print(get_approximate_score(array, pca_result))
+        print('number of dimensions: {}'.format(dim))
+        print('pca score: {}'.format(get_approximate_score(array, pca_result)))
+        print('my simple algorithm score: {} \n'.format(get_approximate_score(array, simple_result)))
+
+
+""" the test results (Surprisingly, my simple algorithm beats PCA up to dimension 9)
+
+number of dimensions: 1
+pca score: 0.32530948814986654
+my simple algorithm score: 0.3286464773846756 
+
+number of dimensions: 2
+pca score: 0.3316649527596166
+my simple algorithm score: 0.3812492238306688 
+
+number of dimensions: 3
+pca score: 0.31839595566998746
+my simple algorithm score: 0.3964351732337903 
+
+number of dimensions: 4
+pca score: 0.33293613699784735
+my simple algorithm score: 0.42310084459058894 
+
+number of dimensions: 5
+pca score: 0.3632980651286149
+my simple algorithm score: 0.4457351576189828 
+
+number of dimensions: 6
+pca score: 0.3770226767955756
+my simple algorithm score: 0.45231211530025417 
+
+number of dimensions: 7
+pca score: 0.3834171794216121
+my simple algorithm score: 0.4592437711307776 
+
+number of dimensions: 8
+pca score: 0.386995813947692
+my simple algorithm score: 0.4775338056576217 
+
+number of dimensions: 9
+pca score: 0.3912612270911561
+my simple algorithm score: 0.4844064545864031 
+"""
